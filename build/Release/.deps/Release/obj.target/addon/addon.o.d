a54089b3ec5e6a2316ad0933af13f4d9849d7474
Pedro <pedrobat1990@gmail.com>
2019-12-20 19:15:10 -0300

    oporra

---
+++ build/Release/.deps/Release/obj.target/addon/addon.o.d
@@ -0,0 +1,11 @@
+cmd_Release/obj.target/addon/addon.o := g++ '-DNODE_GYP_MODULE_NAME=addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/home/labsmart/.cache/node-gyp/13.3.0/include/node -I/home/labsmart/.cache/node-gyp/13.3.0/src -I/home/labsmart/.cache/node-gyp/13.3.0/deps/openssl/config -I/home/labsmart/.cache/node-gyp/13.3.0/deps/openssl/openssl/include -I/home/labsmart/.cache/node-gyp/13.3.0/deps/uv/include -I/home/labsmart/.cache/node-gyp/13.3.0/deps/zlib -I/home/labsmart/.cache/node-gyp/13.3.0/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/addon/addon.o.d.raw   -c -o Release/obj.target/addon/addon.o ../addon.cc
+Release/obj.target/addon/addon.o: ../addon.cc \
+ /home/labsmart/.cache/node-gyp/13.3.0/include/node/node_api.h \
+ /home/labsmart/.cache/node-gyp/13.3.0/include/node/js_native_api.h \
+ /home/labsmart/.cache/node-gyp/13.3.0/include/node/js_native_api_types.h \
+ /home/labsmart/.cache/node-gyp/13.3.0/include/node/node_api_types.h
+../addon.cc:
+/home/labsmart/.cache/node-gyp/13.3.0/include/node/node_api.h:
+/home/labsmart/.cache/node-gyp/13.3.0/include/node/js_native_api.h:
+/home/labsmart/.cache/node-gyp/13.3.0/include/node/js_native_api_types.h:
+/home/labsmart/.cache/node-gyp/13.3.0/include/node/node_api_types.h:
---
+++ server.js
@@ -0,0 +1,4 @@
+const addon = require("./build/Release/addon");
+
+
+console.log(addon.hello({estringue:"ola", valor:123, seila: 10}));
